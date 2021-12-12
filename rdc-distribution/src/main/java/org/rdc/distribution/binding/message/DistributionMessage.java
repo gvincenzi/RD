6f89e0a3cec4451e4d9f1de14ce50d406b5a516c
Giuseppe Vincenzi <gdvincenzi@airfrance.fr>
2021-12-12 20:38:06 +0100

    Sonar refactoring

--- rdc-distribution/src/main/java/org/rdc/distribution/binding/message/DistributionMessage.java
+++ rdc-distribution/src/main/java/org/rdc/distribution/binding/message/DistributionMessage.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 @Data
 public class DistributionMessage<T> {
     private UUID correlationID;
+    String instanceName;
     private DistributionEventType type;
     private T content;
 }
--- rdc-node/src/main/java/org/rdc/node/binding/message/DistributionEventType.java
+++ rdc-node/src/main/java/org/rdc/node/binding/message/DistributionEventType.java
@@ -2,7 +2,5 @@ package org.rdc.node.binding.message;
 
 public enum DistributionEventType {
     ENTRY_PROPOSITION,
-    ENTRY_RESPONSE,
-    LIST_ENTRIES_REQUEST,
-    INTEGRITY_VERIFICATION
+    ENTRY_RESPONSE
 }
