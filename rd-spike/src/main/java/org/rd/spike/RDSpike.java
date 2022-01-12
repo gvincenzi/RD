fa8e7c41475e9211670bce88e9484c6d17ffb548
Giuseppe Vincenzi <gdvincenzi@airfrance.fr>
2022-01-12 17:46:14 +0100

    Spike naming

--- rd-spike/src/main/java/org/rd/spike/RDSpike.java
+++ rd-spike/src/main/java/org/rd/spike/RDSpike.java
@@ -1,4 +1,4 @@
-package org.rdc.spike;
+package org.rd.spike;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
-public class RDCSpike {
+public class RDSpike {
     public static void main(String[] args) {
-        SpringApplication.run(RDCSpike.class, args);
+        SpringApplication.run(RDSpike.class, args);
     }
 }
